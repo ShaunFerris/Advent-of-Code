ef48fa4d64fb2a933ed470706fe35fcd9f902d7f
Shaun Ferris <slbferris46@gmail.com>
2023-01-22 17:45:48 +0100

    Day 2 part 2

--- 2022/2/2.py
+++ 2022/2/2.py
@@ -3,6 +3,7 @@ with open('input.txt', 'r') as f:
 
 GUIDE_KEY = {'A': 'R', 'X': 'R', 'B': 'P', 'Y': 'P', 'C': 'S', 'Z': 'S'}
 RPS_RULES = {'R': 'S','P': 'R', 'S': 'P'}
+RPS_REVERSED = {v: k for k, v in RPS_RULES.items()}
 SCORING = {'R': 1, 'P': 2, 'S': 3, 'W': 6, 'L': 0, 'D': 3}
 
 
@@ -23,3 +24,23 @@ def tournament_A(data):
     return total_score
 
 print(tournament_A(data))
+
+def score_calc_B(opp, result):
+    score = 0
+    if result == 'X': #loss
+        score += SCORING[RPS_RULES[opp]]
+    elif result == 'Y': #draw
+        score += SCORING[opp] + SCORING['D']
+    elif result == 'Z': #win
+        score += SCORING[RPS_REVERSED[opp]] + SCORING['W']
+    return score
+
+def tournament_B(data):
+    total_score = 0
+    for match in data:
+        match = match.split()
+        opp, result = GUIDE_KEY[match[0]], match[1]
+        total_score += score_calc_B(opp, result)
+    return total_score
+
+print(tournament_B(data))
