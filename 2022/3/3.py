852643ff9bbbcd7148d3b8fed4f3eff144a3ed16
Shaun Ferris <slbferris46@gmail.com>
2023-01-22 20:02:56 +0100

    Seperated priority function

--- 2022/3/3.py
+++ 2022/3/3.py
@@ -3,20 +3,27 @@ with open('input.txt', 'r') as f:
     for line in f:
         rucksack_items.append(line.strip())
 
-def rucksack_priorities(rucksack_items):
-    total = 0
-    for item in rucksack_items:
-        compartment_1, compartment_2 = \
-            set(item[: len(item) // 2]), set(item[len(item) // 2 :])
-        duplicates = compartment_1.intersection(compartment_2)
-        ascii = [ord(c) for c in duplicates]
+def get_priority(characters):
+        ascii = [ord(c) for c in characters]
         priorities = []
         for a in ascii:
             if a > 96:
                 priorities.append(a - 96)
             elif a < 91:
                 priorities.append(a - 38)
-        total += sum(priorities)
+        return sum(priorities)
+
+
+def rucksack_priorities(rucksack_items):
+    total = 0
+    for item in rucksack_items:
+        compartment_1, compartment_2 = \
+            set(item[: len(item) // 2]), set(item[len(item) // 2 :])
+        duplicates = compartment_1.intersection(compartment_2)
+        total += get_priority(duplicates)
     return total
 
 print(rucksack_priorities(rucksack_items))
+
+def badges(rucksack_items):
+    pass
