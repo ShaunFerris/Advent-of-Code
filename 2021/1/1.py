94f15b51b5d5e196aaa7320d09fc2898e0df6dd9
Shaun Ferris <slb.ferris46@gmail.com>
2023-01-31 16:30:02 +0000

    Day one part one 2021

--- 2021/1/1.py
+++ 2021/1/1.py
@@ -1,6 +1,18 @@
 with open('input.txt', 'r') as f:
     measurements = []
     for line in f:
-        measurements.append(line.strip())
+        measurements.append(int(line.strip()))
 
-print(measurements)
+depth_delta = {'increase': 0, 'decrease': 0}
+current_depth = 0
+for measurement in measurements:
+    if current_depth == 0:
+        current_depth = measurement
+        continue
+    elif measurement > current_depth:
+        depth_delta['increase'] += 1
+        current_depth = measurement
+    elif measurement < current_depth:
+        depth_delta['decrease'] += 1
+        current_depth = measurement
+print(depth_delta)
