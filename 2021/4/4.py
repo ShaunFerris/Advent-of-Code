8ba5d0583e7f059e583315427cf2c745ec71a253
Shaun Ferris <slb.ferris46@gmail.com>
2023-02-23 15:40:01 +0000

    Complete functions for parsing bingo boards by column and row

--- 2021/4/4.py
+++ 2021/4/4.py
@@ -8,16 +8,37 @@ with open('testinput.txt', 'r') as f:
     boards_data = [row.strip() for row in f.readlines()]
     numbers = [int(num) for num in boards_data.pop(0).split(',')]
 
-def boards_array(bd):
+def rows_by_board(bd):
+    ''''Takes the boards data and processes it into a dictionary where keys are
+    numbered bingo boards and values are lists of the rows, processed to be lists
+    of integers. Each board has 5 rows.'''
+
     n_boards = int(len(bd) / 6)
-    winning_rows = {n: [] for n in range(1, n_boards + 1)}
+    rows = {n: [] for n in range(1, n_boards + 1)}
     board_counter = 0
     for i in bd:
         if i == '':
             board_counter += 1
             continue
         else:
-            winning_rows[board_counter].append([int(num) for num in i.split(' ') if num != ''])
-    return winning_rows
+            rows[board_counter].append([int(num) for num in i.split(' ') if num != ''])
+    return rows
 
-print(boards_array(test_boards_data))
+def columns_by_board(rows):
+    '''Takes the output of rows_by_board and calculates the corresponding vertical
+    columns for each bingo board.'''
+
+    columns = {n: [] for n in range(1, len(rows) + 1)}
+    for board, rws in rows.items():
+        for rw in rws:
+            if rw == rws[0]:
+                for top_num in rw:
+                    columns[board].append([top_num])
+            else:
+                for index, num in enumerate(rw):
+                    columns[board][index].append(num)
+    return columns
+
+rows = rows_by_board(test_boards_data)
+cols = columns_by_board(rows)
+print(f'Rows are:\n{rows}\nColumns are:\n{cols}')
