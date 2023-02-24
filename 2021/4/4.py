65158a6d6092dc5b5158605ccfb8a3e0f6753755
Shaun Ferris <slb.ferris46@gmail.com>
2023-02-24 16:12:00 +0000

    Continuing day one solution, implement winning sequences data struct, board state tracking, check number function

--- 2021/4/4.py
+++ 2021/4/4.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 #process testing dataset
 with open('testinput.txt', 'r') as f:
     test_boards_data = [row.strip() for row in f.readlines()]
@@ -39,6 +41,45 @@ def columns_by_board(rows):
                     columns[board][index].append(num)
     return columns
 
-rows = rows_by_board(test_boards_data)
-cols = columns_by_board(rows)
-print(f'Rows are:\n{rows}\nColumns are:\n{cols}')
+def winning_seqs(bd):
+    '''Combines the column and row parsing functions to build a dictionary of all the winning
+    sequences by board.'''
+
+    rows = rows_by_board(bd)
+    cols = columns_by_board(rows)
+    for board, rws in rows.items():
+        for rw in rws:
+            cols[board].append(rw)
+    winners = cols
+    return winners
+
+def board_states(ws):
+    '''Creates a copy of the winning sets dictionary with 0's in place of the board numbers.
+    Intention is to have these switched to 1's to mark called numbers, and indicate the winning
+    board without working directly in the organised board data dict from winning sets.'''
+
+    states = deepcopy(ws)
+    for seqs in states.values():
+        for seq in seqs:
+            for i in range(len(seq)):
+                seq[i] = 0
+    return states
+
+def check_number(called_num, winning_seqs, board_states):
+    '''This takes a called bingo number, the winning sequences dictionary,
+    and the board state tracking dictionary. It marks the position of the called number,
+    and returns the marked state tracking dict.'''
+
+    for board, seqs in winning_seqs.items():
+        for index_1, seq in enumerate(seqs):
+            for index_2, num in enumerate(seq):
+                if num == called_num:
+                    board_states[board][index_1][index_2] = 1
+    return board_states
+
+
+processed_data = winning_seqs(test_boards_data)
+print(processed_data)
+tracker = board_states(processed_data)
+print('\n', tracker)
+print('\n', check_number(22, processed_data, tracker))
