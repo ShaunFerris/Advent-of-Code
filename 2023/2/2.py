056a29e3812d72339afa07b4aa5a1d3f602947e3
Shaun Ferris <slb.ferris46@gmail.com>
2023-12-06 16:07:16 +0100

    2023 day two part two

--- 2023/2/2.py
+++ 2023/2/2.py
@@ -2,6 +2,7 @@ with open("input.txt") as f:
     lines = [f.strip() for f in f.readlines()]
 
 test_key = {"red": 12, "green": 13, "blue": 14}
+test_game = "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
 
 
 def get_game_id(game):
@@ -29,4 +30,29 @@ def sum_of_legal_games(input_data, key):
     return sum
 
 
-print(sum_of_legal_games(lines, test_key))
+print(f"Part One: {sum_of_legal_games(lines, test_key)}")
+
+
+def min_by_color(game):
+    color_map = {"blue": 0, "red": 0, "green": 0}
+    for item in amounts_by_game(game):
+        if int(item.split()[0]) > color_map[item.split()[-1]]:
+            color_map[item.split()[-1]] = int(item.split()[0])
+    return color_map
+
+
+def power_of_game(game):
+    out = 1
+    for i in min_by_color(game).values():
+        out *= i
+    return out
+
+
+def sum_of_game_powers(input_data):
+    sum = 0
+    for game in input_data:
+        sum += power_of_game(game)
+    return sum
+
+
+print(f"Part Two: {sum_of_game_powers(lines)}")
