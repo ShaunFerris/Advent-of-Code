491a6eac15bc2ed143612818bf5f9c4868a50b39
Shaun Ferris <slb.ferris46@gmail.com>
2023-12-08 11:49:23 +0100

    2023 day four part two complete

--- 2023/4/4.py
+++ 2023/4/4.py
@@ -1,3 +1,5 @@
+from typing import Tuple, List, Dict
+
 with open("input.txt") as f:
     lines = [f.strip() for f in f.readlines()]
 
@@ -11,14 +13,15 @@ example = [
 ]
 
 
-def process_card(card_line: str):
+def process_card(card_line: str) -> Dict[str, List[int]]:
     return {
+        "game_id": int(card_line.split(":")[0].split(" ")[-1]),
         "winning": [int(i) for i in card_line.split(":")[1].split("|")[0].split()],
         "held": [int(i) for i in card_line.split(":")[1].split("|")[1].split()],
     }
 
 
-def total_score_of_card(card_line: str):
+def total_score_of_card(card_line: str) -> int:
     total = 0
     processed = process_card(card_line)
     for i in processed["held"]:
@@ -30,11 +33,30 @@ def total_score_of_card(card_line: str):
     return total
 
 
-def solve(input_data):
+def solve(input_data: List[str]) -> int:
     out = 0
     for line in input_data:
         out += total_score_of_card(line)
     return out
 
 
-print(solve(lines))
+print(f"Part One: {solve(lines)}")
+
+
+def wins_by_id(card_line: str) -> Tuple[int, int]:
+    game_id, winning, held = process_card(card_line).values()
+    number_of_wins = len(set(winning).intersection(set(held)))
+    return (game_id, number_of_wins)
+
+
+def solve_duplicating_cards(input_data: List[str]) -> int:
+    counter = {process_card(card)["game_id"]: 1 for card in input_data}
+    for card in input_data:
+        game_id, number_of_wins = wins_by_id(card)
+        for _ in range(counter[game_id]):
+            for i in range(game_id + 1, game_id + number_of_wins + 1):
+                counter[i] += 1
+    return sum(counter.values())
+
+
+print(f"Part Two: {solve_duplicating_cards(lines)}")
