0ebd9efdaccfb174cc81a6a8a4dfb85377f5ddd3
Shaun Ferris <slb.ferris46@gmail.com>
2023-12-12 11:04:09 +0100

    String processing function for day six 2023

--- 2023/6/6.py
+++ 2023/6/6.py
@@ -1,11 +1,18 @@
 import re
+from typing import List, Tuple
 
 with open("input.txt") as f:
     lines = [f.strip() for f in f.readlines()]
 
 
-def process_race_data(raw_data):
-    return re.findall("\d+", raw_data[1])
+def process_race_data(raw_data: List[str]) -> List[Tuple[int]]:
+    time_line, distance_line = raw_data
+    times = re.findall("\d+", time_line)
+    distances = re.findall("\d+", distance_line)
+    processed = []
+    for i in range(len(times)):
+        processed.append((times[i], distances[i]))
+    return processed
 
 
 print(process_race_data(lines))
