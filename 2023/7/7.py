92e46ad24535308d090bd405cb3d929954dca2e6
Shaun Ferris <slb.ferris46@gmail.com>
2023-12-14 09:54:15 +0100

    2023 day seven part two

--- 2023/7/7.py
+++ 2023/7/7.py
@@ -12,11 +12,11 @@ class Hand:
     # Lookup for tuple representations of handtypes, better hand higher value
     hand_types: Dict[Tuple, int] = {
         (5,): 7,  # five of a kind
-        (4, 1): 6,  # four of a kind etc...
-        (3, 2): 5,  # full house
-        (3, 1, 1): 4,
-        (2, 2, 1): 3,
-        (2, 1, 1, 1): 2,
+        (1, 4): 6,  # four of a kind etc...
+        (2, 3): 5,  # full house
+        (1, 1, 3): 4,
+        (1, 2, 2): 3,
+        (1, 1, 1, 2): 2,
         (1, 1, 1, 1, 1): 1,
     }
 
@@ -40,10 +40,39 @@ class Hand:
     def __init__(self, hand: str):
         self.hand, self.bid = hand.split()
         self.bid = int(self.bid)
-        self.hand_type = self.get_hand_type(hand)
+        self.hand_type = self.get_hand_type()
 
-    def process_hand(line: str) -> Tuple[str, int]:
-        return (line.split()[0], int(line.split()[1]))
+    def __gt__(self, other) -> bool:
+        if self.hand_type > other.hand_type:
+            return True
+        elif self.hand_type == other.hand_type:
+            for i in range(len(self.hand)):
+                if self.hand[i] == other.hand[i]:
+                    continue
+                else:
+                    return (
+                        self.card_types[self.hand[i]] > self.card_types[other.hand[i]]
+                    )
+        else:
+            return False
 
-    def get_hand_type(self, processed_hand: Tuple[str, int]) -> int:
-        return self.hand_types[tuple(Counter(processed_hand[0]).values())]
+    def __eq__(self, other) -> bool:
+        if self.hand_type == other.hand_type:
+            return True
+        else:
+            return False
+
+    def get_hand_type(self) -> int:
+        return self.hand_types[tuple(sorted(Counter(self.hand).values()))]
+
+
+def solve(input_data: List[str]) -> int:
+    winnings = 0
+    hands = [Hand(line) for line in input_data]
+    hands.sort()
+    for index, hand in enumerate(hands):
+        winnings += (index + 1) * hand.bid
+    return winnings
+
+
+print(f"Part One: {solve(lines)}")
