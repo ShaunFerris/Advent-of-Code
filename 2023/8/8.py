b6a106a3123a0ba30d359e60e4dbb4e240ab166f
Shaun Ferris <slb.ferris46@gmail.com>
2023-12-15 09:28:32 +0100

    2023 day eight part one complete

--- 2023/8/8.py
+++ 2023/8/8.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import re
 
 with open("input.txt") as f:
@@ -8,7 +8,7 @@ with open("subset.txt") as f:
     test_lines = [f.strip() for f in f.readlines()]
 
 
-class navigation_loop:
+class NavigationLoop:
     def __init__(self, navigation_instructions: str):
         self.instructions = navigation_instructions
         self.index = 0
@@ -29,7 +29,7 @@ class navigation_loop:
 test_instructions = test_lines[0]
 
 
-def process_nav_map(input_data: List[str]):
+def process_nav_map(input_data: List[str]) -> Tuple[str, Dict[str, Tuple[str]]]:
     """
     Process the input data. Arg: raw input data as a list of lines.
     Returns: a tuple with the navigation instruction string at idx 0 and a dict that maps nodes
@@ -38,7 +38,7 @@ def process_nav_map(input_data: List[str]):
     navigation_instructions = input_data[0]
     raw_nav_map = input_data[2:]
     nav_map = {
-        processed_line[0]: tuple(
+        processed_line[0].strip(): tuple(
             ("").join(re.findall(r"[A-Z]", item))
             for item in processed_line[1].split(",")
         )
@@ -48,4 +48,24 @@ def process_nav_map(input_data: List[str]):
     return (navigation_instructions, nav_map)
 
 
-print(process_nav_map(test_lines))
+def chart_course(input_data: List[str]) -> int:
+    """
+    Calculate the number of steps from node AAA to node ZZZ following the instructions in the
+    provided navigation loop
+    Arg: raw input data as a list of lines
+    Returns: Number of steps between nodes as an int
+    """
+    nav_instructions, nav_map = process_nav_map(input_data=input_data)
+    nav_loop = NavigationLoop(nav_instructions)
+    instruction_key = {"R": 1, "L": 0}
+    current_node = "AAA"
+    step_count = 0
+    for instruction in nav_loop:
+        current_node = nav_map[current_node][instruction_key[instruction]]
+        step_count += 1
+        if current_node == "ZZZ":
+            break
+    return step_count
+
+
+print(f"Part One: {chart_course(lines)}")
